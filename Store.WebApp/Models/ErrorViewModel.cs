5853b2141049871a8a5e4eeea2666a65105eb60e
harelfishbein <harelfishbein@github.com>
2020-11-19 15:28:19 -0800

    Created Folders from templates

---
+++ Store.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
