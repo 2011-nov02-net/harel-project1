95612e8ebdc1ef51ebde07434658731569f69156
harelfishbein <harelfishbein@github.com>
2020-11-24 10:23:08 -0800

    added logging and connection string

--- Store.WebApp/Startup.cs
+++ Store.WebApp/Startup.cs
@@ -12,10 +12,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Store.DataModel;
 
+using System.IO;
+using Microsoft.Extensions.Logging;
+
 namespace Store.WebApp
 {
     public class Startup
     {
+        const string connectionStringPath = "../connectionString.txt";
         public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Environment = env;
@@ -25,14 +29,19 @@ namespace Store.WebApp
         public IConfiguration Configuration { get; }
         public IWebHostEnvironment Environment { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
+        static string GetConnectionString(string path)
+        {
+            string connectionString;
+            connectionString = File.ReadAllText(path);
+            return connectionString;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<Project1Context>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
-            services.AddScoped<ISession, Session>(services => {
+            services.AddSingleton<ISession, Session>(services => {
                 var optionsBuilder = new DbContextOptionsBuilder<Project1Context>();
-                optionsBuilder.UseSqlServer<Project1Context>();
-                //optionsBuilder.UseLoggerFactory();
+                optionsBuilder.UseSqlServer<Project1Context>(GetConnectionString(connectionStringPath));
+                using var logStream = new StreamWriter("ef-logs.txt");
+                optionsBuilder.LogTo(logStream.WriteLine, LogLevel.Information);
                 return new Session(optionsBuilder.Options);
             });
             services.AddMvc(option => option.EnableEndpointRouting = false);
